282f2a46a6254020276b23f87d07ec353c676850
Vishal Girase <67498551+vishgirase95@users.noreply.github.com>
2021-07-26 23:55:05 +0530

    uploaded all

---
+++ index.js
@@ -0,0 +1,14 @@
+ function price() {
+
+    alert(" This book cost 150 rs , Please Click to Procced");
+    
+     
+ }
+
+//  document.getElementById("#buyme").function () {("click",
+
+//  alert("This book cost 150rs , Please click to proced"));
+     
+//  }
+ 
+ 
